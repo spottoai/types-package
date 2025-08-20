4e1165e748a269d7ac5193238f4154c2a77cb210
Jay Ji <jiqiang90@hotmail.com>
2025-08-20 15:35:20 +1200

    add user

---
+++ src/accounts/user.ts
@@ -0,0 +1,25 @@
+export interface BaseUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface User extends BaseUser {
+  companyId: string;
+  role: number;
+  isPendingInvite: boolean;
+  invitedBy: string;
+}
+
+export interface UserProfile extends BaseUser {
+  Timestamp: string;
+  criticalAlertsEnabled: boolean;
+  emailNotificationsEnabled: boolean;
+  language: string;
+  marketingNotificationsEnabled: boolean;
+  monthlyReportsEnabled: boolean;
+  newRecommendationsNotify: boolean;
+}
