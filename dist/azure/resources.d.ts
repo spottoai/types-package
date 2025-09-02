0a15c9f33951ec38fdf5eaffe470a3d7c08d57f7
Jay Ji <jiqiang90@hotmail.com>
2025-09-02 14:45:24 +1200

    update resources

--- dist/azure/resources.d.ts
+++ dist/azure/resources.d.ts
@@ -2,21 +2,33 @@ export interface ResourcesByType {
     type: string;
     resources: number;
     spendBilling?: number;
+    spendBillingAmortized?: number;
     spendPreviousBilling?: number;
+    spendPreviousBillingAmortized?: number;
     spend30Days?: number;
+    spend30DaysAmortized?: number;
     spendPrevious30Days?: number;
+    spendPrevious30DaysAmortized?: number;
     spend7Days?: number;
+    spend7DaysAmortized?: number;
     spendPrevious7Days?: number;
+    spendPrevious7DaysAmortized?: number;
 }
 export interface ResourceByLocation {
     location: string;
     resources: number;
     spendBilling?: number;
+    spendBillingAmortized?: number;
     spendPreviousBilling?: number;
+    spendPreviousBillingAmortized?: number;
     spend30Days?: number;
+    spend30DaysAmortized?: number;
     spendPrevious30Days?: number;
+    spendPrevious30DaysAmortized?: number;
     spend7Days?: number;
+    spend7DaysAmortized?: number;
     spendPrevious7Days?: number;
+    spendPrevious7DaysAmortized?: number;
 }
 export interface ResourceTypeReference {
     type: string;
