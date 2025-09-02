ef4595a7cdcae9a9289b2e6821fc195186cbdeb6
Jay Ji <jiqiang90@hotmail.com>
2025-09-02 14:45:06 +1200

    update resources

--- src/azure/resources.ts
+++ src/azure/resources.ts
@@ -2,22 +2,34 @@ export interface ResourcesByType {
   type: string;
   resources: number;
   spendBilling?: number; // total spend in the current billing period
+  spendBillingAmortized?: number; // total amortized spend in the current billing period
   spendPreviousBilling?: number; // total spend in the previous billing period
+  spendPreviousBillingAmortized?: number;
   spend30Days?: number; // total spend over the last 30 days
+  spend30DaysAmortized?:number;
   spendPrevious30Days?: number; // total spend over the previous 30 days
+  spendPrevious30DaysAmortized?: number;
   spend7Days?: number; // total spend over the last 7 days
+  spend7DaysAmortized?: number;
   spendPrevious7Days?: number; // total spend over the previous 7 days
+  spendPrevious7DaysAmortized?: number;
 }
 
 export interface ResourceByLocation {
   location: string;
   resources: number;
   spendBilling?: number; // total spend in the current billing period
+  spendBillingAmortized?: number; // total amortized spend in the current billing period
   spendPreviousBilling?: number; // total spend in the previous billing period
+  spendPreviousBillingAmortized?: number;
   spend30Days?: number; // total spend over the last 30 days
+  spend30DaysAmortized?:number;
   spendPrevious30Days?: number; // total spend over the previous 30 days
+  spendPrevious30DaysAmortized?: number;
   spend7Days?: number; // total spend over the last 7 days
+  spend7DaysAmortized?: number;
   spendPrevious7Days?: number; // total spend over the previous 7 days
+  spendPrevious7DaysAmortized?: number;
 }
 
 export interface ResourceTypeReference {
