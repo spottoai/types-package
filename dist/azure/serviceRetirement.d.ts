baf849c14e67de12dfccf580c85f0d2ed8c8f5f6
JayStrydom <jay.strydom.developer@gmail.com>
2025-10-18 11:36:23 +1300

    Kick the build to get a new version

---
+++ dist/azure/serviceRetirement.d.ts
@@ -0,0 +1,24 @@
+import { ServiceRetirementRecommendation } from './recommendations';
+export interface ServiceRetirement {
+    id: string;
+    subscriptionId: string;
+    resourceId: string;
+    resourceName: string;
+    resourceType: string;
+    resourceGroup: string;
+    location: string;
+    retiringFeature: string;
+    retirementDate: string;
+    effort?: 'high' | 'medium' | 'low';
+    effortHours?: number;
+    tags?: Record<string, string>;
+}
+export interface ServiceRetirementPortalResource {
+    id: string;
+    name?: string;
+    resourceType?: string;
+}
+export interface ServiceRetirementPortalEntry extends ServiceRetirementRecommendation {
+    resources: ServiceRetirementPortalResource[];
+}
+//# sourceMappingURL=serviceRetirement.d.ts.map
