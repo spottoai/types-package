ca81ce5faf3a4507e0cf5314016670e6d8629ed7
JayStrydom <jay.strydom.developer@gmail.com>
2025-11-19 18:42:30 +1300

    Added billing account summary object

--- src/company/billingAccount.ts
+++ src/company/billingAccount.ts
@@ -8,3 +8,24 @@ export interface BillingAccount {
   stripeItemExcessId: string;
   stripeItemLicensedId: string;
 }
+
+export interface BillingAccountSummary {
+  managedByParent: boolean;
+  parentCompany?: {
+    id: string;
+    name: string;
+    billingAccountId?: string;
+  };
+  message?: string;
+  billingAccount?: BillingAccount;
+}
+
+export interface BillingAccountPostRequest {
+  billingEmail?: string;
+  returnUrl?: string;
+}
+
+export interface BillingAccountPortalSession {
+  billingAccount: BillingAccount;
+  portalSessionUrl: string;
+}
